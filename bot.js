da3b728ddfc3bc796e08789f12c792f766fe8176
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-05-29 00:17:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -104,11 +104,11 @@ function postMessage(botResponse, attachments, botID) {
   console.log('sending ' + botResponse + ' to ' + botID);
 
   botReq = HTTPS.request(options, function(res) {
-   //   if (res.statusCode == 202) {
-        //neat
-    //  } else {
-     //   console.log('rejecting bad status code ' + res.statusCode);
-   //   }
+    if (res.statusCode == 200) {
+     //  neat
+      } else {
+       console.log('rejecting bad status code ' + res.statusCode);
+     }
   });
 
   botReq.on('error', function(err) {
